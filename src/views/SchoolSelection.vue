5dcb18ccfc6b3cf375d9a78a2ac49beecaf37662
joesuffredini <gsuffred@gmail.com>
2021-04-27 17:32:13 -0400

    Updated the modal for displaying schools selected by user, added images to the modal

--- src/views/SchoolSelection.vue
+++ src/views/SchoolSelection.vue
@@ -9,27 +9,27 @@
     <form method="dialog">
       <h1>School selection filter options:</h1>
       <div class="form-group">
-        <label for="enrollment">Enrollment range:</label>
+        <label for="enrollment">School size:</label>
         <input type="range" v-model="range" min="5000" max="80000" step="500" />
         Selected: {{ range }}
       </div>
       <br />
-      <div class="form-group">
+      <!-- <div class="form-group">
         <label>Academics:</label>
         <input type="academics" class="form-control" v-model="academics" />
-      </div>
+      </div> -->
       <br />
       <div class="form-group">
-        <label>Total number of recruits:</label>
+        <label>Recruit #'s:</label>
         <input type="recruittotal" class="form-control" v-model="recruittotal" />
       </div>
       <br />
-      <button v-on:click="indexSchools(range, academics, recruittotal)">Submit your filters</button>
+      <button v-on:click="indexSchools(range, recruittotal)">Submit your filters</button>
     </form>
     <br />
     <br />
     <form v-on:submit.prevent="createSchoolSelection()">
-      <h1>Select your school to add to profile</h1>
+      <!-- <h1>Select your school to add to profile</h1> -->
       <div class="form-group">
         <label>School</label>
         <select class="form-control" v-model="school">
@@ -78,10 +78,10 @@ export default {
     indexSchools: function (range, recruittotal) {
       axios.get("/api/school").then((response) => {
         var temp = response.data.filter(function (x) {
-          return x.enrollment < range || x.recruittotal < recruittotal;
+          return x.enrollment < range && x.recruits_position.length <= recruittotal;
         });
         this.schools = temp;
-        console.log(this.schools);
+        console.log("filtered data", this.schools);
       });
     },
   },
--- src/views/UserShow.vue
+++ src/views/UserShow.vue
@@ -12,12 +12,30 @@
       <h3>Position: {{ user.position }} Shoots: {{ user.shoots }}</h3>
       <h3>Current Coach: {{ user.coach }}</h3>
       <h3>Email: {{ user.email }}</h3>
+      <!-- <img :src="user.test" :alt="user.image" width="150" height="150" /> -->
     </div>
+
+    <!-- <div v-for="school in schools" :key="school">
+      <router-link v-bind:to="`user/${user.id}`">
+        <h3>MySchool: {{ school }}</h3>
+      </router-link>
+    </div> -->
+
+    <!-- <h1>My School Choices</h1>
+    <div v-for="image in images" :key="image">
+      <router-link v-bind:to="`user/${user.id}`">
+        <img :src="image" :alt="image" width="150" height="150" />
+      </router-link>
+    </div> -->
 
     <button v-on:click="showSchools()">View Schools Selected</button>
     <dialog id="school-info">
       <form method="dialog">
-        <h3>Schools: {{ user.schools }}</h3>
+        <div v-for="image in images" :key="image">
+          <router-link v-bind:to="`/school/`">
+            <img :src="image" :alt="image" width="75" height="75" />
+          </router-link>
+        </div>
         <button>Close</button>
       </form>
     </dialog>
@@ -30,6 +48,8 @@ export default {
   data: function () {
     return {
       user: {},
+      schools: {},
+      images: {},
     };
   },
   created: function () {
@@ -38,8 +58,11 @@ export default {
   methods: {
     showUser: function () {
       axios.get("/api/user/" + localStorage.getItem("user_id")).then((response) => {
-        console.log(response.data);
+        console.log("user info", response.data);
         this.user = response.data;
+        this.schools = this.user.schools;
+        this.images = this.user.image;
+        console.log(this.user.schools);
       });
     },
 
