a61fb9e26846e051d9bc080ade3a6e91f6cb393a
joesuffredini <gsuffred@gmail.com>
2021-04-29 17:50:13 -0400

    Fixed login page with form layout looking nice

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -10,8 +10,8 @@
         </ul>
 
         <div class="mb-3 row">
-          <label for="inputEmail" class="col-sm-2 col-form-label"></label>
-          <div class="col-sm-10">
+          <label for="inputEmail" class="col-sm-4 col-form-label"></label>
+          <div class="col-sm-4">
             <input
               type="email"
               class="form-control"
@@ -23,8 +23,8 @@
           </div>
         </div>
         <div class="mb-3 row">
-          <label for="inputPassword" class="col-sm-2 col-form-label"></label>
-          <div class="col-sm-10">
+          <label for="inputPassword" class="col-sm-4 col-form-label"></label>
+          <div class="col-sm-4">
             <input
               type="password"
               class="form-control"
